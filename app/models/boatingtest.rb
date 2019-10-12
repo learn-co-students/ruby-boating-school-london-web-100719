2d4d898667610f4bd16e76ac58c6c6e6e80d43b7
Ned-el-ch <nedelchev.niki@gmail.com>
2019-10-12 20:51:53 +0100

    passed all the requirements and added functionality to lookup the passing and failing students by test name

--- app/models/boatingtest.rb
+++ app/models/boatingtest.rb
@@ -2,14 +2,17 @@ require 'pry'
 
 class BoatingTest
 
-    attr_reader :name
-    attr_accessor
+    attr_reader :name, :instructor, :student
+    attr_accessor :status
 
     @@all = []
 
-    def initialize(name)
+    def initialize(name, instructor, student, status = "Pending")
 
         @name = name
+        @status = status
+        @instructor = instructor
+        @student = student
 
         BoatingTest.all << self
         
@@ -18,6 +21,18 @@ class BoatingTest
     def self.all
 
         @@all
+        
+    end
+
+    def self.passing_students(test_name)
+
+        BoatingTest.all.select {|t| t.name == test_name}.select {|t| t.status == "Passed"}.map {|t| t.student}
+        
+    end
+
+    def self.failing_students(test_name)
+
+        BoatingTest.all.select {|t| t.name == test_name}.select {|t| t.status == "Failed"}.map {|t| t.student}
         
     end
 
--- app/models/instructor.rb
+++ app/models/instructor.rb
@@ -21,4 +21,45 @@ class Instructor
         
     end
 
+    def tests
+
+        BoatingTest.all.select {|boating_test| boating_test.instructor == self}
+        
+    end
+
+    def pass_student(student, test_name)
+
+        valid_test = self.tests.select {|boating_test| boating_test.name == test_name && boating_test.student == student}[0]
+
+        if !valid_test
+
+            BoatingTest.new(test_name, self, student, "Passed")
+            
+            
+        else
+
+            valid_test.status = "Passed"
+            
+        end
+
+    end
+
+    def fail_student(student, test_name)
+
+        valid_test = self.tests.select {|boating_test| boating_test.name == test_name && boating_test.student == student}[0]
+
+        if !valid_test
+
+            BoatingTest.new(test_name, self, student, "Failed")
+            
+            
+        else
+
+            valid_test.status = "Failed"
+            valid_test
+            
+        end
+
+    end
+
 end
