232406bcf07081ee903618d81be868246dc9d472
gortron <gordon.lanza@gmail.com>
2019-10-13 11:38:28 +0100

    solved initial methods

--- app/models/instructor.rb
+++ app/models/instructor.rb
@@ -1,3 +1,36 @@
 class Instructor
+  attr_accessor :name
+  @@all = []
+
+  def initialize(name)
+    @name = name
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
+
+  def find_test(student, test_name)
+    # This will return either a BoatingTest instance, or nil if there is no BoatingTest instance for the given student and test_name.
+    BoatingTest.all.select { |test| test.student == student ** test.test_name == test_name }[0]
+  end
+
+  def pass_student(student, test_name)
+    # If there is a test with this student and test name, then pass the student
+    if find_test(student, test_name)
+      find_test(student, test_name).test_status = 'passed'
+    else
+      BoatingTest.new(student, test_name, 'passed', self)
+    end
+  end
+  
+  def fail_student(student, test_name)
+    if find_test(student, test_name)
+      find_test(student, test_name).test_status = 'failed'
+    else
+      BoatingTest.new(student, test_name, 'failed', self)
+    end
+  end
 
 end
